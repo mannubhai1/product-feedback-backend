af632a8fd93ad910f088383b4f4ee82bddfe700e
Manurbhav Arya <cs21b057@iittp.ac.in>
2025-03-30 23:13:44 +0530

    upate config for deployment

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,9 @@
 import * as dotenv from "dotenv";
-dotenv.config({ path: ".env.test" });
+if (process.env.NODE_ENV === "test") {
+  dotenv.config({ path: ".env.test" });
+} else if (!process.env.OPENAI_API_KEY) {
+  dotenv.config();
+}
 
 export const config = {
   port: process.env.PORT || 3000,
